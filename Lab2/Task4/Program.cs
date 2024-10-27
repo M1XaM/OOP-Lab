a4345fb67fc930d975952b7af11f450298a998bd
Isacescu Maxim <isacescu.maxim12@gmail.com>
2024-10-27 17:42:04 +0200

    Task4 completed

---
+++ Lab2/Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System;
+using FileReaderNamespace;
+using TextDataNamespace;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        if(args.Length == 0)
+        {
+            Console.WriteLine("Input Error: a file must be passed");
+            return;
+        }
+        else
+        {
+            foreach (string arg in args)
+            {
+                string filePath = arg;
+                string text = FileReader.readFileIntoString(filePath);      
+                TextData textData = new TextData(text);
+                textData.setFileName(filePath);     
+                Console.WriteLine(textData.getFilename());
+                Console.WriteLine(textData.getNumberOfVowels());
+                Console.WriteLine(textData.getNumberOfConsonants());
+                Console.WriteLine(textData.getNumberOfLetters());
+                Console.WriteLine(textData.getNumberOfSenteces());
+                Console.WriteLine(textData.getLongestWord());
+                Console.WriteLine();
+            }
+        }
+    }
+}
