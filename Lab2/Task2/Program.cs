2713fc9c3457cb95334fbf87e96638ba02255ac7
Isacescu Maxim <isacescu.maxim12@gmail.com>
2024-11-02 19:05:49 +0200

    Task2 finished

---
+++ Lab2/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CoffeeNamespace;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cappuccino myDrink = new Cappuccino(Intensity.NORMAL, 100);
+        // Americano myDrink = new Americano(Intensity.STRONG, 100);
+        // PumpkinSpiceLatte myDrink = new PumpkinSpiceLatte(Intensity.LIGHT, 100, 200);
+        // SyrupCappuccino myDrink = new SyrupCappuccino(Intensity.LIGHT, 100, SyrupType.CARAMEL);
+        myDrink.printDetails();
+    }
+}
